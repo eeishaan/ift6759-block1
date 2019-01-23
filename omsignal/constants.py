e89a4adb4281b4f56f24f66d2305404c0b292e15
Ishaan Kumar <ffishaan@gmail.com>
2019-01-22 20:19:28 -0500

    corrected data root dir var

--- omsignal/constants.py
+++ omsignal/constants.py
@@ -5,7 +5,7 @@ from pathlib import Path
 CURR_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 RESULT_DIR = CURR_DIR / 'results'
 
-DATA_ROOT_DIR = Path('omsignal/fakedata')
-# ROOT_DIR: '/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/'
+# DATA_ROOT_DIR = Path('omsignal/fakedata')
+DATA_ROOT_DIR = Path('/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
 TRAIN_LABELED_FILE = DATA_ROOT_DIR / 'MILA_TrainLabeledData.dat'
 VALIDATION_LABELED_FILE = DATA_ROOT_DIR / 'MILA_ValidationLabeledData.dat'
