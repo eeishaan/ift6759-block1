6eadfb862418d5a36c31903168cd8cf90a210c86
Ishaan Kumar <ffishaan@gmail.com>
2019-01-25 16:00:57 -0500

    added spectogram and FFT transformers

--- omsignal/utils/transform.py
+++ omsignal/utils/transform.py
@@ -3,7 +3,9 @@
 Module containing data transformation
 '''
 
+import numpy as np
 import torch
+from scipy import signal
 
 
 class ToTensor(object):
@@ -28,3 +30,62 @@ class ToNumpy(object):
 
     def __call__(self, X):
         return X.numpy()
+
+
+class FFT(object):
+    '''
+    Fast fourier trasnform
+    '''
+
+    def __init__(self, signal_ndim=1, normalized=False):
+        self.signal_ndim = signal_ndim
+        self.normalized = normalized
+
+    def __call__(self, X):
+        # add a new dimesion and fill it with zero
+        X.unsqueeze_(2)
+        X.index_fill_(1, torch.tensor([2]), 0)
+        out = torch.fft(X, 1)
+        return out
+    
+
+class Spectogram(object):
+    '''
+    Spectogram transform
+    '''
+
+    def __init__(self, lognorm: bool=False, fs: int=16,
+                 nperseg: int=256, noverlap: int=None):
+        '''Takes a time-series x and returns the spectogram
+            Args:
+                lognorm: log spectrogram or not. Defaults to ``False``
+                fs: Sampling frequency of the x time series.
+                    Defaults to 16.
+                nperseg: Length of each segment. Defaults to 256.
+                noverlap: Number of points to overlap between segments.
+                        If None, ``noverlap = nperseg // 8``. Defaults to None.
+
+            Returns :
+                  f (ndarray): Array of sample frequencies.
+                  t (ndarray): Array of segment times.
+                  Zxx (ndarray): Spectrogram of x.
+                    By default, the last axis of Zxx corresponds
+                    to the segment times.
+        '''
+        self.lognorm = lognorm
+        self.fs = fs
+        self.nperseg = nperseg
+        self.noverlap = noverlap
+
+    def __call__(self, X):
+        f, t, Zxx = signal.spectrogram(
+            X, fs=self.fs, nperseg=self.nperseg, noverlap=self.noverlap
+        )
+
+        if self.lognorm:
+            Zxx = np.abs(Zxx)
+            mask = Zxx > 0
+            Zxx[mask] = np.log(Zxx[mask])
+            Zxx = (Zxx - np.min(Zxx)) / (np.max(Zxx) - np.min(Zxx))
+
+        return f, t, Zxx
