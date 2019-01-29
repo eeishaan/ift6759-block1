a26a0d8efed1e5a50b18bf8116500cd10c72a20e
Ishaan Kumar <ffishaan@gmail.com>
2019-01-29 13:04:03 -0500

    dirty commit, added code for lstm training

--- omsignal/__main__.py
+++ omsignal/__main__.py
@@ -13,6 +13,7 @@ from torchvision.transforms import Compose
 
 from omsignal.constants import (RESULT_DIR, TRAIN_LABELED_FILE,
                                 VALIDATION_LABELED_FILE)
+from omsignal.model import LSTMModel
 from omsignal.utils.augmentation import SignalShift
 from omsignal.utils.dim_reduction import SVDTransform, TSNETransform
 from omsignal.utils.loader import (OmsignalDataset, get_dataloader,
@@ -101,17 +102,19 @@ def dim_reduction_task(train_vectors, train_labels, out_dir):
 def train_simple_model():
     device = torch.device("cpu")
     # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    remap_transform = RemapLabels()
     transform = Compose([
-        RemapLabels(),
+        remap_transform,
+        # SignalShift(shift_len=3000),
         ToTensor()
     ])
     train_dataset = OmsignalDataset(
         TRAIN_LABELED_FILE,
         augment_transform=SignalShift(shift_len=3750),
         transform=transform)
-    train_dataloader = get_dataloader(train_dataset, num_workers=4)
+    train_dataloader = get_dataloader(train_dataset, num_workers=0)
     validation_dataset = OmsignalDataset(
-        VALIDATION_LABELED_FILE, transform=ToTensor())
+        VALIDATION_LABELED_FILE, transform=transform)
     validation_dataloader = get_dataloader(validation_dataset)
     D_in, h_1, h_2, D_out = 3750, 1024, 512, 32
     model = nn.Sequential(
@@ -128,9 +131,10 @@ def train_simple_model():
     for e in range(epochs):
         model.train()
         running_loss = 0
-        for i, sample in enumerate(train_dataloader):
+        for _, sample in enumerate(train_dataloader):
             raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
-            
+            print(raw_data.shape)
+            continue
             # zero the parameter gradients
             optimizer.zero_grad()
             
@@ -139,18 +143,81 @@ def train_simple_model():
             running_loss += loss.item()
             loss.backward()
             optimizer.step()
-        print('Epoch : %d Loss : %.3f ' % (e, running_loss/len(train_dataloader)))
+        # model.eval()
+        # correct = 0
+        # for i, sample in enumerate(validation_dataloader):
+        #     raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
+        #     outputs = model(raw_data)
+        #     _, predicted = torch.max(outputs.data, 1)
+        #     print(set(labels[:, -1].cpu().numpy()) - set(predicted.cpu().numpy()))
+        #     correct += recall_score(labels[:, -1].cpu().numpy(), predicted.cpu().numpy(), average='macro')
+
+        # if e % 10 == 9:
+        #     print('Epoch : %d Loss : %.3f ' % (e, running_loss/len(train_dataloader)))
+        #     print('Epoch : %d Validation Score: %.3f' % (e, correct))
+
+
+
+def train_lstm():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    remap_transform = RemapLabels()
+    transform = Compose([
+        remap_transform,
+        ToTensor()
+    ])
+    
+    # initialize train dataloader
+    train_dataset = OmsignalDataset(
+        TRAIN_LABELED_FILE,
+        augment_transform=SignalShift(shift_len=3750),
+        transform=transform)
+    train_dataloader = get_dataloader(train_dataset, num_workers=0)
+    
+    # initialize validation dataloader
+    validation_dataset = OmsignalDataset(
+        VALIDATION_LABELED_FILE, transform=transform)
+    validation_dataloader = get_dataloader(validation_dataset)
+    
+    D_in, h_1, D_out = 3750, 512, 32
+
+    # initialize LSTM model
+    model = LSTMModel(D_in, D_out, h_1).to(device)
+
+    criterion = nn.NLLLoss()
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.7)
+
+    epochs = 100
+    for e in range(epochs):
+        model.train()
+        running_loss = 0
+        for _, sample in enumerate(train_dataloader):
+            raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
+
+            # erase LSTM history
+            model.init_hidden()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
+            outputs = model(raw_data)
+            loss = criterion(outputs, labels[:, -1])
+            running_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
         model.eval()
         correct = 0
-        for i, sample in enumerate(validation_dataloader):
+        for _, sample in enumerate(validation_dataloader):
             raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
             outputs = model(raw_data)
             _, predicted = torch.max(outputs.data, 1)
+            print(set(labels[:, -1].cpu().numpy()) - set(predicted.cpu().numpy()))
             correct += recall_score(labels[:, -1].cpu().numpy(), predicted.cpu().numpy(), average='macro')
-        
-        print('Epoch : %d Validation Score: %.3f' % (e, correct))
-        exit()
 
+        if e % 10 == 9:
+            print('Epoch : %d Loss : %.3f ' % (e, running_loss/len(train_dataloader)))
+            print('Epoch : %d Validation Score: %.3f' % (e, correct))
 
 def main():
     '''
@@ -163,7 +230,9 @@ def main():
     # turn off interactive plotting
     plt.ioff()
 
-    train_simple_model()
+    torch.manual_seed(1)
+    train_lstm()
+    # train_simple_model()
     # separate out the labels and raw data
     # train_vectors, train_labels = get_vector_and_labels(TRAIN_LABELED_FILE)
 
