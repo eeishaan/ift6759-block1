2d27da8fbbef434f7668a0ae4b1cc8026a371d96
Ishaan Kumar <ffishaan@gmail.com>
2019-01-28 20:44:44 -0500

    dirty commit for a simple model

--- omsignal/__main__.py
+++ omsignal/__main__.py
@@ -5,13 +5,18 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import torch.nn as nn
 import yaml
+from sklearn.metrics import recall_score
 from torchvision.transforms import Compose
 
 from omsignal.constants import (RESULT_DIR, TRAIN_LABELED_FILE,
                                 VALIDATION_LABELED_FILE)
+from omsignal.utils.augmentation import SignalShift
 from omsignal.utils.dim_reduction import SVDTransform, TSNETransform
-from omsignal.utils.loader import get_vector_and_labels
+from omsignal.utils.loader import (OmsignalDataset, get_dataloader,
+                                   get_vector_and_labels)
 from omsignal.utils.preprocessor import Preprocessor
 from omsignal.utils.transform import ToNumpy, ToTensor
 
@@ -93,6 +98,56 @@ def dim_reduction_task(train_vectors, train_labels, out_dir):
     _reduce_and_plot(train_vectors, train_labels, tsne_transform)
     
 
+def train_simple_model():
+    device = torch.device("cpu")
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    train_dataset = OmsignalDataset(TRAIN_LABELED_FILE)
+    validation_dataset = OmsignalDataset(VALIDATION_LABELED_FILE)
+    transform = Compose([
+        SignalShift(),
+        ToTensor()
+    ])
+    train_dataloader = get_dataloader(train_dataset, transform)
+    validation_dataset = OmsignalDataset(VALIDATION_LABELED_FILE)
+    validation_dataloader = get_dataloader(validation_dataset, ToTensor())
+    D_in, h_1, h_2, D_out = 3750, 1024, 512, 32
+    model = nn.Sequential(
+        Preprocessor(),
+        nn.Linear(D_in, h_1),
+        nn.ReLU(),
+        nn.Linear(h_1, h_2),
+        nn.ReLU(),
+        nn.Linear(h_2, D_out)
+    ).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
+    epochs = 100
+    for e in range(epochs):
+        model.train()
+        running_loss = 0
+        for i, sample in enumerate(train_dataloader):
+            raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
+            outputs = model(raw_data)
+            loss = criterion(outputs, labels[:, -1])
+            running_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+        print('Epoch : %d Loss : %.3f ' % (e, running_loss/len(train_dataloader)))
+        model.eval()
+        correct = 0
+        for i, sample in enumerate(validation_dataloader):
+            raw_data, labels = sample[:, :-4].to(device), sample[:, -4:].to(device).long()
+            outputs = model(raw_data)
+            _, predicted = torch.max(outputs.data, 1)
+            correct += recall_score(labels[:, -1], predicted, average='macro')
+        
+        print('Epoch : %d Validation Score: %.3f' % (e, correct))
+        exit()
+
 
 def main():
     '''
@@ -105,17 +160,21 @@ def main():
     # turn off interactive plotting
     plt.ioff()
 
+    train_simple_model()
     # separate out the labels and raw data
-    train_vectors, train_labels = get_vector_and_labels(TRAIN_LABELED_FILE)
-    valid_vectors, valid_labels = get_vector_and_labels(VALIDATION_LABELED_FILE)
+    # train_vectors, train_labels = get_vector_and_labels(TRAIN_LABELED_FILE)
+    # valid_vectors, valid_labels = get_vector_and_labels(VALIDATION_LABELED_FILE)
 
-    task1_output_folder = RESULT_DIR / 'task1' 
-    os.makedirs(task1_output_folder, exist_ok=True)
-    task1(train_vectors, train_labels, task1_output_folder)
+    # sample = np.hstack((train_vectors, train_labels))
+    # tr = SignalShift()
+    # print(tr(sample).shape)
+    # task1_output_folder = RESULT_DIR / 'task1' 
+    # os.makedirs(task1_output_folder, exist_ok=True)
+    # task1(train_vectors, train_labels, task1_output_folder)
 
-    scatter_plot_dir = RESULT_DIR / 'dim_reduction'
-    os.makedirs(scatter_plot_dir, exist_ok=True)
-    dim_reduction_task(train_vectors, train_labels, scatter_plot_dir)
+    # scatter_plot_dir = RESULT_DIR / 'dim_reduction'
+    # os.makedirs(scatter_plot_dir, exist_ok=True)
+    # dim_reduction_task(train_vectors, train_labels, scatter_plot_dir)
 
 
 if __name__ == '__main__':
