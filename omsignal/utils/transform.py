dd6016a8d3a2727624e56b01bb99ef263dc48018
Ishaan Kumar <ffishaan@gmail.com>
2019-02-02 09:16:21 -0500

    added signal segmenter transform

--- omsignal/utils/transform.py
+++ omsignal/utils/transform.py
@@ -102,3 +102,106 @@ class Spectogram(object):
             Zxx = (Zxx - np.min(Zxx)) / (np.max(Zxx) - np.min(Zxx))
 
         return f, t, Zxx
+
+
+class SignalSegmenter(object):
+
+    def __init__(self, R_peak, segment_size=110, all_window=True):
+        self.segment_size = segment_size
+        self.all_window = all_window
+
+    @classmethod
+    def detect_R_peak(data, SADA_wd_size = 7, FS_wd_size = 12, Threshold = 35):
+        """
+        Take a Batch of ECG data and find the location of the R Peak
+        
+        The algorithm is based on the paper:
+        Online and Offline Determination of QT and PR Interval and QRS Duration in Electrocardiography
+        (Bachler et al., 2012)
+        The variable name and default value follow the paper
+        
+        Parameters
+        ----------
+        data : numpy array
+            The ECG Data (batch size x lenght of the ECG recording)
+        SADA_wd_size: int
+            size of the moving window used in the calculation of SA and DA
+        FS_wd_size: int
+            size of the moving window used in the calculation of the feature signal FS
+        Threshold: int
+            FS is compared to the Threshold to determined if its a QRS zone. 
+        """
+        R_peak = []
+        
+        #Allow batch size of 1
+        if len(data.size()) == 1:
+            data = data.unsqueeze(0)
+        
+        D = data[:, 1:] - data[:, 0:-1]
+        
+        
+        data = data.unsqueeze(0)
+        D = D.unsqueeze(0)
+        SA = F.max_pool1d(data, kernel_size = SADA_wd_size, stride = 1)
+        SA = SA + F.max_pool1d(-data, kernel_size = SADA_wd_size, stride = 1) 
+        DA = F.max_pool1d(D, kernel_size = SADA_wd_size, stride = 1, padding=1)
+        DA = DA + F.max_pool1d(-D, kernel_size = SADA_wd_size, stride = 1, padding=1) 
+        
+        C = DA[:,:,1:] * torch.pow(SA, 2)
+        FS = F.max_pool1d(C, kernel_size = FS_wd_size, stride = 1) 
+        Detect = (FS > Threshold)
+        
+        Detect = Detect.squeeze(0).cpu()
+        data = data.squeeze(0).cpu()
+
+        for ECG in range(len(data)):
+            
+            in_QRS = 0
+            start_QRS = 0
+            end_QRS = 0
+            r_peak = np.array([])
+            
+            for tick, detect in enumerate(Detect[ECG]):
+                
+                if (in_QRS == 0) and (detect == 1):
+                    start_QRS = tick
+                    in_QRS = 1
+                    
+                elif (in_QRS == 1) and (detect == 0):
+                    end_QRS = tick
+                    R_tick = torch.argmax(data[ECG, start_QRS : end_QRS+SADA_wd_size+FS_wd_size]).item()
+                    r_peak = np.append(r_peak, R_tick + start_QRS)
+                    in_QRS = 0
+                    start_QRS = 0
+                    
+            R_peak.append(r_peak)
+            
+        return R_peak
+
+    def __call__(self, data):
+        R_peak = SignalSegmenter.detect_R_peak(data)
+
+        listoftemplate = np.empty((1,self.segment_size))
+        
+        for recording in range(len(R_peak)):
+            #generate the template
+            half_size_int = int(self.segment_size//2)
+            template = np.zeros((1,self.segment_size))
+            
+            for i in R_peak[recording][1:-1]:
+                new_heart_beat = data[recording][int(i)-int(half_size_int*0.8): int(i)+int(half_size_int*1.2)]
+                #add padding to make them all of the same size
+                current_size = int(half_size_int*0.8)+int(half_size_int*1.2)
+                
+                    
+                template = np.concatenate((template,
+                                        np.expand_dims(new_heart_beat, axis = 0))
+                                        , axis=0)
+
+            if self.all_window == False:
+                template = np.mean(template, axis = 0)
+                template = np.expand_dims(template, axis = 0)
+            
+            listoftemplate = np.append(listoftemplate, template, axis = 0)
+        
+        return listoftemplate[1:]
