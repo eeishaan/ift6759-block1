a02527771eecb31651e8bc85c7e03cf97169c69c
Ishaan Kumar <ffishaan@gmail.com>
2019-02-02 18:23:03 -0500

    added some constants

--- omsignal/constants.py
+++ omsignal/constants.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 CURR_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 RESULT_DIR = CURR_DIR / 'results'
+MODEL_DIR = Path("/rap/jvb-000-aa/COURS2019/etudiants/user20/ift6759")
+MODEL_PATH = MODEL_DIR / 'model.pt'
 
 # DATA_ROOT_DIR = Path('omsignal/fakedata')
 DATA_ROOT_DIR = Path('/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
