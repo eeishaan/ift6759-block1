bc917a9dd59e9272f2f32ee6421cae7a0b40b1ef
Ishaan Kumar <ffishaan@gmail.com>
2019-02-02 20:38:00 -0500

    added deep cnn model

--- omsignal/model.py
+++ omsignal/model.py
@@ -78,3 +78,89 @@ class CNNClassifier(nn.Module):
         x = x.view(len(x), -1)
         x = self.linear(x)
         return x
+
+
+class DeepCNNClassifier(nn.Module):
+    def __init__(self, n_filters=32, kernel_size=3, linear_dim=51):
+        super(DeepCNNClassifier, self).__init__()
+
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.linear_dim = linear_dim
+        self.cnn = nn.Sequential(
+            nn.Conv1d(
+                in_channels=1,
+                out_channels=64,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(64),
+            
+            nn.Conv1d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(64),
+            
+            nn.MaxPool1d(2),
+
+            nn.Conv1d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(128),
+
+            nn.Conv1d(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(128),
+
+            nn.MaxPool1d(2),
+
+            nn.Conv1d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(256),
+
+            nn.Conv1d(
+                in_channels=256,
+                out_channels=256,
+                kernel_size=self.kernel_size
+            ),
+            nn.ELU(),
+            nn.BatchNorm1d(256),
+
+            nn.MaxPool1d(2),
+
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(2048, 32),
+            nn.ELU(),
+            nn.BatchNorm1d(2048),
+            nn.Dropout(0.5),
+        )
+        self.cnn.apply(CNNClassifier.init_weights)
+        self.linear.apply(CNNClassifier.init_weights)
+
+    @classmethod
+    def init_weights(cls, m):
+        if isinstance(m ,(nn.Linear, nn.Conv1d)):
+            torch.nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.cnn(x)
+        x = x.view(len(x), -1)
+        x = self.linear(x)
+        return x
