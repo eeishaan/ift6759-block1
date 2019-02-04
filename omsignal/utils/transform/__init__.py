17b1636f04a9d3fbea340586de29bdd40b2c6b97
Ishaan Kumar <ffishaan@gmail.com>
2019-02-03 22:24:30 -0500

    made __call__ abstractmethod for transformers

--- omsignal/utils/transform/__init__.py
+++ omsignal/utils/transform/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class OmTransform(ABC):
     '''
     Abstract base class for custom transformers
     '''
+
+    @abstractmethod
+    def __call__(self, x):
+        pass
+
 
     def state(self):
         '''
