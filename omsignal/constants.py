6463437668c1bf4652ad4c0ddbec470655e35be3
Ishaan Kumar <ffishaan@gmail.com>
2019-02-04 19:44:15 -0500

    updated constants

--- omsignal/constants.py
+++ omsignal/constants.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from pathlib import Path
 
 
@@ -6,6 +6,10 @@ CURR_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 RESULT_DIR = CURR_DIR / 'results'
 
 # DATA_ROOT_DIR = Path('omsignal/fakedata')
-DATA_ROOT_DIR = Path('/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
+DATA_ROOT_DIR = Path(
+    '/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
 TRAIN_LABELED_FILE = DATA_ROOT_DIR / 'MILA_TrainLabeledData.dat'
 VALIDATION_LABELED_FILE = DATA_ROOT_DIR / 'MILA_ValidationLabeledData.dat'
+
+
+MODEL_DIR = CURR_DIR / "saved_models"
