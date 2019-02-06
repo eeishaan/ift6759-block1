ba0dc69ac0f1cceadec9fa32927ef09256fa5b29
Ishaan Kumar <ffishaan@gmail.com>
2019-02-05 23:57:19 -0500

    fixing seed before import

--- omsignal/__main__.py
+++ omsignal/__main__.py
@@ -2,10 +2,10 @@
 import numpy
 import torch
 
-from omsignal.runner import run_cnn_exp, run_regression_exp
-
 numpy.random.seed(42)
 torch.manual_seed(42)
+
+from omsignal.runner import run_cnn_exp, run_regression_exp
 
 
 if __name__ == '__main__':
