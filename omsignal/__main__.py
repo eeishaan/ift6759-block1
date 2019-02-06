bb10cf1cbcd92ff54c57d18a70a2d59c3d0d8401
Ishaan Kumar <ffishaan@gmail.com>
2019-02-05 21:43:26 -0500

    added seeds

--- omsignal/__main__.py
+++ omsignal/__main__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import numpy
-numpy.random.seed(1)
 import torch
-torch.manual_seed(1)
 
 from omsignal.runner import run_cnn_exp, run_regression_exp
+
+numpy.random.seed(42)
+torch.manual_seed(42)
+
 
 if __name__ == '__main__':
     run_regression_exp()
