2f886ba488affbf3606e0f9ef70fb1ce1d2678b5
NBerty <39064188+NBerty@users.noreply.github.com>
2019-02-06 10:42:30 -0500

    Update lstm.py
    Updated the LSTM model.

--- omsignal/models/lstm.py
+++ omsignal/models/lstm.py
@@ -6,9 +6,21 @@ import torch.nn.functional as F
 
 from omsignal.utils.preprocessor import Preprocessor
 
+class gaussian_noise(nn.Module):
+    def __init__(self, std=0.1, is_relative_detach=True):
+        super().__init__()
+        self.std = std
+        self.is_relative_detach = is_relative_detach
+
+    def forward(self, x):
+        if self.training and self.std != 0:
+            scale = self.std*x.detach() if self.is_relative_detach else self.std*x
+            sampled_noise = torch.randn(x.shape).to(device)*scale
+            x = x + sampled_noise
+        return x
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_dim, out_dim, hidden_dim, device):
+    def __init__(self, input_dim, out_dim, hidden_dim, n_layers, device):
         super(LSTMModel, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
@@ -16,22 +28,36 @@ class LSTMModel(nn.Module):
 
         self.device = device
 
+        self.noise = gaussian_noise()
+
         self.preprocessor = Preprocessor()
         self.lstm = nn.LSTM(input_size=self.input_dim,
                             hidden_size=self.hidden_dim,
+                            num_layers=self.n_layers,
                             batch_first=True)
 
-        self.linear = nn.Linear(self.hidden_dim, self.out_dim)
+        self.dropout1 = nn.Dropout(p=0.40)
+        self.linear1 = nn.Linear(self.hidden_dim, int(self.hidden_dim/2))
+        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(p=0.20)
+        self.linear2 = nn.Linear(int(self.hidden_dim/2), out_dim)
 
-    def init_hidden(self):
-        self.hidden = (torch.zeros(1, 20, self.hidden_dim).to(self.device),
-                       torch.zeros(1, 20, self.hidden_dim).to(self.device))
+    def init_hidden(self, batch_size):
+        self.hidden = (torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device),
+                       torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device))
 
-    def forward(self, X):
-        X = self.preprocessor(X)
-        lstm_out, self.hidden = self.lstm(
-            X.view(len(X), -1, 1), self.hidden)
-        lstm_out = lstm_out.contiguous()[:, -1, :]
-        tag_space = self.linear(lstm_out.view(20, -1))
-        tag_scores = F.log_softmax(tag_space, dim=1)
-        return tag_scores
+    def forward(self, inputs, hidden, batch_size):
+        #LSTM
+        inputs = inputs.view(batch_size, inputs.shape[1], inputs.shape[2])
+        inputs = self.noise(inputs)
+        inputs, hidden = self.lstm(inputs, hidden)
+
+        #MLP
+        inputs = inputs.contiguous()[:, -1, :]
+        inputs = self.dropout1(inputs)
+        inputs = self.linear1(inputs.view(batch_size, -1))
+        inputs = self.relu(inputs)
+        inputs = self.dropout2(inputs)
+        output = self.linear2(inputs)
+
+        return output, hidden
