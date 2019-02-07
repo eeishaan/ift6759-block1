2ffb43185a181909c2af383a934ce41f7c9a672c
Ishaan Kumar <ffishaan@gmail.com>
2019-02-07 12:38:22 -0500

    function to get dataloader for preprocessed data

--- omsignal/utils/loader.py
+++ omsignal/utils/loader.py
@@ -6,6 +6,7 @@ Data loading routines
 from torch.utils.data import DataLoader, Dataset
 
 from omsignal.utils.memfile import read_memfile
+from omsignal.utils.transform.preprocessor import get_preprocessed_data
 
 
 class OmsignalDataset(Dataset):
@@ -13,9 +14,9 @@ class OmsignalDataset(Dataset):
     Omsignal dataset class
     '''
 
-    def __init__(self, file_path,
-                 shape=(160, 3754), dtype='float32', transform=None):
-        self.data = read_memfile(file_path, shape=shape, dtype=dtype)
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
         self.transform = transform
 
     def __len__(self):
@@ -23,17 +24,39 @@ class OmsignalDataset(Dataset):
 
     def __getitem__(self, idx):
         sample = self.data[idx, :]
+        label = self.labels[idx, :]
         if self.transform:
             sample = self.transform(sample)
-        return sample
+        return sample, label
 
 
-def get_dataloader(dataset, shuffle=True, batch_size=20, num_workers=4):
-    '''
-    Construct a dataloader
-    '''
-    return DataLoader(dataset, batch_size=batch_size,
-                      shuffle=shuffle, num_workers=num_workers)
+def get_dataloader(
+        data_file_path,
+        label_type,
+        remap_transform,
+        only_ids,
+        segmenter_cls,
+        shuffle,
+        batch_size):
+    # load data
+    train_data, train_labels = get_vector_and_labels(data_file_path)
+
+    # get_preprocess_data
+    train_data, train_labels, row_id_map = get_preprocessed_data(
+        train_data,
+        train_labels,
+        only_ids,
+        remap_transform,
+        segmenter_cls
+    )
+
+    # make dataloader
+    train_data = torch.Tensor(train_data)
+    train_labels = label_type(train_labels)
+    train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=shuffle)
+    return train_loader, row_id_map
 
 
 def get_vector_and_labels(file_path: str) -> tuple:
