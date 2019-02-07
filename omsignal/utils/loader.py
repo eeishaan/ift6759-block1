ee39600d3928b9ce6f66a6d73f08988c09785ea9
Ishaan Kumar <ffishaan@gmail.com>
2019-02-07 18:41:03 -0500

    function for constructing test dataloader

--- omsignal/utils/loader.py
+++ omsignal/utils/loader.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader, Dataset
 
 from omsignal.utils.memfile import read_memfile
 from omsignal.utils.transform.preprocessor import get_preprocessed_data
+from omsignal.utils.misc import check_file
+from omsignal.constants import DATA_ROOT_DIR
 
 
 class OmsignalDataset(Dataset):
@@ -58,6 +60,32 @@ def get_dataloader(
     loader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=shuffle)
     return loader, row_id_map
+
+
+def get_test_dataloader(test_data_file, segmenter, batch_size):
+    test_data_file = check_file(test_data_file, DATA_ROOT_DIR)
+    if test_data_file is None:
+        return
+    # load test data
+    test_data = read_memfile(
+        test_data_file, shape=(160, 3750), dtype='float32')
+
+    # pre-process
+    test_data, row_mapping = get_preprocessed_data(
+        data=test_data,
+        labels=None,
+        only_ids=False,
+        remap_label_transformer=None,
+        segmenter=segmenter,
+    )
+
+    # make dataloader
+    test_data = torch.Tensor(test_data)
+    dataset = torch.utils.data.TensorDataset(test_data)
+    loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=False
+    )
+    return loader, row_mapping
 
 
 def get_vector_and_labels(file_path: str) -> tuple:
