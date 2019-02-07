d9fbf320eda3e209b451da044ceb9467775fe174
Ishaan Kumar <ffishaan@gmail.com>
2019-02-07 11:06:21 -0500

    check if file exist in test script

--- omsignal/test.py
+++ omsignal/test.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
+import os
+import sys
+
+from omsignal.constants import DATA_ROOT_DIR, MODEL_DIR
+from omsignal.test import MODEL_EXP_MAP
+
+logger = logging.getLogger(__name__)
 
 
 def get_test_parser(parent=None):
@@ -9,6 +17,13 @@ def get_test_parser(parent=None):
     else:
         parser = parent.add_parser('test', help='Test pre-trained models')
 
+    parser.add_argument(
+        '--model',
+        type=str,
+        help='Type of model to evaluate',
+        choices=MODEL_EXP_MAP.keys(),
+        required=True,
+    )
     parser.add_argument(
         '--model-path',
         type=str,
@@ -23,3 +38,35 @@ def get_test_parser(parent=None):
     )
 
     return parser
+
+
+def _check_file(file_path, default_path):
+    if not os.path.isfile(file_path):
+        message = 'Unable to find model at {}. Trying at default location.'\
+            .format(file_path)
+        logger.info(message)
+        file_path = default_path / file_path
+        if not os.path.isfile(file_path):
+            message = 'Unable to find model at {}'\
+                .format(file_path)
+            logger.error(message)
+            return None
+    return file_path
+
+
+def run_eval(args):
+    data_file = args.data_file
+    model_file = args.model_file
+    model_file = _check_file(model_file, MODEL_DIR)
+    if model_file is None:
+        return
+    data_file = _check_file(data_file, DATA_ROOT_DIR)
+    if data_file is None:
+        return
+    # exp = MODEL_EXP_MAP[args.map]
+
+
+if __name__ == '__main__':
+    parser = get_test_parser()
+    args = parser.parse_args()
+    run_eval(args)
