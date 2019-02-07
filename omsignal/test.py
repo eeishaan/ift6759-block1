5723e9eb287b49d4d13b0de8ef86342f9f89761f
Ishaan Kumar <ffishaan@gmail.com>
2019-02-07 15:24:59 -0500

    removed redundant information

--- omsignal/test.py
+++ omsignal/test.py
@@ -7,8 +7,7 @@ import sys
 
 from omsignal.constants import DATA_ROOT_DIR, MODEL_DIR
 from omsignal.train import MODEL_EXP_MAP
-
-logger = logging.getLogger(__name__)
+from omsignal.utils.misc import check_file
 
 
 def get_test_parser(parent=None):
@@ -40,27 +39,13 @@ def get_test_parser(parent=None):
     return parser
 
 
-def _check_file(file_path, default_path):
-    if not os.path.isfile(file_path):
-        message = 'Unable to find model at {}. Trying at default location.'\
-            .format(file_path)
-        logger.info(message)
-        file_path = default_path / file_path
-        if not os.path.isfile(file_path):
-            message = 'Unable to find model at {}'\
-                .format(file_path)
-            logger.error(message)
-            return None
-    return file_path
-
-
 def run_eval(args):
     data_file = args.data_file
     model_file = args.model_file
-    model_file = _check_file(model_file, MODEL_DIR)
+    model_file = check_file(model_file, MODEL_DIR)
     if model_file is None:
         return
-    data_file = _check_file(data_file, DATA_ROOT_DIR)
+    data_file = check_file(data_file, DATA_ROOT_DIR)
     if data_file is None:
         return
     # exp = MODEL_EXP_MAP[args.map]
