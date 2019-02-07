04d001ff2c0592d5aeffde0a81ffb00daf3b6570
Ishaan Kumar <ffishaan@gmail.com>
2019-02-07 15:41:12 -0500

    added a const

--- omsignal/constants.py
+++ omsignal/constants.py
@@ -5,6 +5,7 @@ CURR_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 LOG_FILE_INI = CURR_DIR / 'logging_config.ini'
 MODEL_DIR = CURR_DIR / "saved_models"
 RESULT_DIR = CURR_DIR / 'results'
+PARAM_DIR = CURR_DIR / 'params'
 
 DATA_ROOT_DIR = Path(
     '/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
