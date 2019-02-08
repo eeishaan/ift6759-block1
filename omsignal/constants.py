32c7b3cd4de11a3c6e175a28cdaf9fc40e51eb55
Ishaan Kumar <ffishaan@gmail.com>
2019-02-08 01:05:54 -0500

    corrected constant spelling

--- omsignal/constants.py
+++ omsignal/constants.py
@@ -6,7 +6,7 @@ LOG_FILE_INI = CURR_DIR / 'logging_config.ini'
 MODEL_DIR = CURR_DIR / "saved_models"
 RESULT_DIR = CURR_DIR / 'results'
 PARAM_DIR = CURR_DIR / 'params'
-ID_MAPPING = CURR_DIR / 'lable_mapping.json'
+ID_MAPPING = CURR_DIR / 'label_mapping.json'
 DATA_ROOT_DIR = Path(
     '/rap/jvb-000-aa/COURS2019/etudiants/data/omsignal/myHeartProject/')
 TRAIN_LABELED_FILE = DATA_ROOT_DIR / 'MILA_TrainLabeledData.dat'
